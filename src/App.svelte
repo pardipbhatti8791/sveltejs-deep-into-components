baa8c8b339836aada70f34e1936a44a86e50b2e0
Pardip Bhatti <pardipbhatti8791@gmail.com>
2020-06-20 12:27:55 +0530

    extract data from custom event

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,13 @@
 <script>
 	import Product from './Product.svelte'
+
+	function addToCart(e) {
+		console.log(e.detail)
+	}
 </script>
 
 <Product 
 	productTitle="Meri Gugu" 
-	on:add-to-cart="{(e) => console.log(e)}"
-	on:delete="{() => alert('deleted')}"
+	on:add-to-cart="{addToCart}"
+	on:delete="{(e) => console.log(e.detail)}"
 />
