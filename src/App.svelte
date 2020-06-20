0d79460aaf83a82124fcf9ca239e126977a6c512
Pardip Bhatti <pardipbhatti8791@gmail.com>
2020-06-20 12:32:22 +0530

    passing props one by one

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,24 @@
 <script>
 	import Product from './Product.svelte'
 
+	let products = [
+		{
+			id: 'p1',
+			title: 'Apple Ipad',
+			price: 1000
+		}
+	]
+
 	function addToCart(e) {
 		console.log(e.detail)
 	}
 </script>
 
+{#each products as product}
 <Product 
-	productTitle="Meri Gugu" 
+	title={product.title} 
+	price={product.price}
 	on:add-to-cart="{addToCart}"
 	on:delete="{(e) => console.log(e.detail)}"
 />
+{/each}
--- src/Product.svelte
+++ src/Product.svelte
@@ -1,7 +1,8 @@
 <script>
 import { createEventDispatcher } from 'svelte'
 
-export let productTitle;
+export let title;
+export let price;
 const dispatch = createEventDispatcher();
 function addToCart() {
   dispatch('add-to-cart', { id: 'p1' })
@@ -9,7 +10,8 @@ function addToCart() {
 </script>
 
 <article>
-  <h1>{productTitle}</h1>
+  <h1>{title}</h1>
+  <h2>${price}</h2>
   <button on:click="{addToCart}">Add to Card</button>
   <button on:click="{() => dispatch('delete', 'p1')}">Delete</button>
 </article>
